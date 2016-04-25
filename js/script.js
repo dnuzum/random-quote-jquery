8bb9b60858008fc520ee4f7f3b5779b0e898375f
dnuzum <derek.nuzum@gmail.com>
2016-04-25 11:38:06 -0700

    fix commits

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
-  //code here
+var chuck = $.getJSON("./js/jokes.json").done(function(chuck) {
+	console.log(chuck.value);
+	var chuckJokes = chuck.value.map(function(jokes) {
+		return(jokes.joke);
+	});
+
+	var num = Math.floor((Math.random() * 546) + 1);
+
+	var say = (chuckJokes[num]);
+	// console.log(say);
+
+	$('#generate-joke').on('submit', function(e) {
+		e.preventDefault();
+		console.log('click');
+		$('#joke').text(say);
+	});
+// function getRandom()
+
+// $(document).ready(function() {
+//   $
+// });
 });
